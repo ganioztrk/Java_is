7a839904718eee248877281212cda4688e5513e9
ganioztrk <gani97905@gmail.com>
2021-09-14 22:49:26 +0300

    guncelleme

---
+++ workspace/A_Harf-Cizimleri/src/main.java
@@ -0,0 +1,13 @@
+/*
+E F G H K L N P S
+*/
+
+public class main {
+    public static void main(String[] args) {
+        for(int i = 1 ; i <= 4 ; i++){
+            for(int j = 1 ; j<= 4 ; j ++){
+                System.out.print(" * ");
+            }
+        }
+    }
+}
