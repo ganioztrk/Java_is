f09206a0561d1ccf6d2eafc9aa8751a646901392
ganioztrk <gani97905@gmail.com>
2021-09-16 17:59:53 +0300

    16,09

--- workspace/A_Harf-Cizimleri/src/main.java
+++ workspace/A_Harf-Cizimleri/src/main.java
@@ -1,13 +1,47 @@
 /*
-E F G H K L N P S
+E F G L  H K N P S
 */
 
 public class main {
     public static void main(String[] args) {
-        for(int i = 1 ; i <= 4 ; i++){
-            for(int j = 1 ; j<= 4 ; j ++){
-                System.out.print(" * ");
+        /*int k = 9;
+        for(int i = 1 ; i <= k ; i++){
+            for(int j = 1 ; j<= k ; j ++){
+                if(i==1 || j==1 || i == k || (i == (k/2)+1 && j< (k / 2)+ 1)){
+                   // System.out.print(i+""+j+" ");
+                    System.out.print(" *");
+                }
             }
+                System.out.println("");
+        }*/
+        
+        // ------------------------------------------
+        
+        /*int k = 9;
+        for(int i = 1 ; i <= k ; i++){
+            for(int j = 1 ; j<= k ; j ++){
+                if(i==1 || j==1 ||(i == (k/2)+1 && j< (k / 2)+ 1)){
+                   // System.out.print(i+""+j+" ");
+                    System.out.print(" *");
+                }
+            }
+                System.out.println("");
+        }*/
+        
+        //-------------------------------------------
+        
+        int k = 9;
+        for(int i = 1 ; i <= k ; i++){
+            for(int j = 1 ; j<= k ; j ++){
+                if(i == 1 || j == 1 || i == k || j == k ){
+                   // System.out.print(i+""+j+" ");
+                      System.out.print(" *");
+                }
+                else{
+                    System.out.print("  ");
+                }    
+            }
+               System.out.println();
         }
     }
 }
