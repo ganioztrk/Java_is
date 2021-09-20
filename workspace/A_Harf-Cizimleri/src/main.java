a3a7c7daf5d3d91cd8b21e5a15cdb1fb3585a55f
ganioztrk <gani97905@gmail.com>
2021-09-20 18:05:37 +0300

    20/09

--- workspace/A_Harf-Cizimleri/src/main.java
+++ workspace/A_Harf-Cizimleri/src/main.java
@@ -30,16 +30,17 @@ public class main {
         
         //-------------------------------------------
         
-        int k = 9;
+        
+        int k = 13;
         for(int i = 1 ; i <= k ; i++){
             for(int j = 1 ; j<= k ; j ++){
-                if(i == 1 || j == 1 || i == k || j == k ){
-                   // System.out.print(i+""+j+" ");
+                if(j == k/2 || ( i + j == k && i <= k/2) || (i - j == 1 && j >= k/2)){
+                //  System.out.print(i+""+j+" ");
                       System.out.print(" *");
                 }
                 else{
                     System.out.print("  ");
-                }    
+                }
             }
                System.out.println();
         }
